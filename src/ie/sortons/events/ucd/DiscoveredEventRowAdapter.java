31ee8500b696775dea3a8dcd5caa476795b19176
Brian Henry <brian.henry2@gmail.com>
2013-11-04 17:52:38 +0000

    Basics working

--- src/ie/sortons/events/ucd/DiscoveredEventRowAdapter.java
+++ src/ie/sortons/events/ucd/DiscoveredEventRowAdapter.java
@@ -1,12 +1,14 @@
 package ie.sortons.events.ucd;
 
 import java.util.List;
+
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.appspot.sortonsevents.upcomingEvents.model.DiscoveredEvent;
@@ -27,9 +29,13 @@ public class DiscoveredEventRowAdapter extends ArrayAdapter<DiscoveredEvent> {
 		protected TextView name;
 		protected TextView time;
 		protected TextView location;
+		protected ImageView picture;
 	}
 
 
+	/* (non-Javadoc)
+	 * @see android.widget.ArrayAdapter#getView(int, android.view.View, android.view.ViewGroup)
+	 */
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View view = null;
@@ -40,7 +46,7 @@ public class DiscoveredEventRowAdapter extends ArrayAdapter<DiscoveredEvent> {
 			viewHolder.name = (TextView) view.findViewById(R.id.eventrowname);
 			viewHolder.time = (TextView) view.findViewById(R.id.eventrowtime);
 			viewHolder.location = (TextView) view.findViewById(R.id.eventrowlocation);
-				
+			viewHolder.picture = (ImageView) view.findViewById(R.id.eventpicture);
 			view.setTag(viewHolder);
 			
 		} else {
@@ -51,6 +57,15 @@ public class DiscoveredEventRowAdapter extends ArrayAdapter<DiscoveredEvent> {
 		holder.time.setText(upcomingEvents.get(position).getFbEvent().getStartTime());
 		holder.location.setText(upcomingEvents.get(position).getFbEvent().getLocation());
 		
+//		URL imageUrl = null;
+//		try {
+//			imageUrl = new URL("//graph.facebook.com/" + upcomingEvents.get(position).getFbEvent().getEid() + "/picture?type=square");
+//		} catch (MalformedURLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		holder.picture.setImageURL(imageUrl);
+//		
 		return view;
 	}
 } 
