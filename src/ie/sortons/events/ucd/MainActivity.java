3bad8288aa3cfc9db8194b7024c73f084f4c61af
Brian Henry <brian.henry2@gmail.com>
2013-11-24 19:29:56 +0000

    Fragment added

--- src/ie/sortons/events/ucd/MainActivity.java
+++ src/ie/sortons/events/ucd/MainActivity.java
@@ -11,8 +11,11 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.Menu;
+import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,11 +28,13 @@ import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.json.gson.GsonFactory;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 	DBTools dbTools = new DBTools(this);
 	ProgressDialog dialog;
 	TextView txtMessage;
 	ListView eventslistview;
+
+	ViewGroup mapFrame; // (frame)
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +44,31 @@ public class MainActivity extends Activity {
 		queryCloudEndpoint();
 
 		eventslistview = (ListView) findViewById(R.id.events_list);
+
+
+		// Restore state
+		if (savedInstanceState != null) {
+			// The fragment manager will handle restoring them if we are being
+			// restored from a saved state
+		}
+		// If this is the first creation of the activity, add fragments to it
+		else {
+
+			// If our layout has a container for the image selector fragment,
+			// create and add it
+			mapFrame = (ViewGroup) findViewById(R.id.map_frame);
+			if (mapFrame != null) {
+				Log.i("oncreate", "onCreate: adding ImageSelectorFragment to MainActivity");
+
+				// Add image selector fragment to the activity's container layout
+				MapFragment mapFragment = new MapFragment();
+				FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+				fragmentTransaction.replace(mapFrame.getId(), mapFragment, MapFragment.class.getName());
+
+				// Commit the transaction
+				fragmentTransaction.commit();
+			}
+		}
 
 	}
 
@@ -70,7 +100,7 @@ public class MainActivity extends Activity {
 					DiscoveredEventCollection pojo = builder.build().upcomingEventsEndpoint().getList(params[0]).execute();
 
 					Log.i("Asd", pojo.toPrettyString());
-					
+
 					return pojo.getItems();
 
 				} catch (IOException e) {
@@ -82,7 +112,7 @@ public class MainActivity extends Activity {
 			@Override
 			public void onPostExecute(List<DiscoveredEvent> data) {
 				txtMessage.setText("");
-				
+
 				Log.i("onPostExecute", "endpoint returned "+data.size());
 				ArrayList<HashMap<String,String>> eventsToBeAdded = new ArrayList<HashMap<String,String>>();
 				ArrayList<HashMap<String,String>> pagesToBeAdded = new ArrayList<HashMap<String, String>>();
@@ -97,7 +127,7 @@ public class MainActivity extends Activity {
 					entry.put("startTime", e.getStartTime());
 					entry.put("latitude", e.getLatitude().toString());
 					entry.put("longtitude", e.getLongitude().toString());
-					
+
 					for(int j = 0; j < data.get(i).getSourcePages().size(); j++) {
 						FbPage p = data.get(i).getSourcePages().get(j);
 						HashMap<String, String> page = new HashMap<String, String>(); 
@@ -107,13 +137,13 @@ public class MainActivity extends Activity {
 						page.put("pageUrl", p.getPageUrl());
 						pagesToBeAdded.add(page);
 					}
-					
+
 					eventsToBeAdded.add(entry);
 				}
 				dbTools.insertEvents(eventsToBeAdded);
 				dbTools.insertSourcePages(pagesToBeAdded);
 				dbTools.deleteAllEventsBeforeDate(new Date());
-				
+
 				showList(data);
 
 				dialog.dismiss();
@@ -128,5 +158,5 @@ public class MainActivity extends Activity {
 		eventslistview.setAdapter(adapter);
 
 	}
-	
+
 }
